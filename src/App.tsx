cbe810cc95918a9fff9038d0993e7ea4c0d310ba
Aarron Lee <aclee91@gmail.com>
2020-09-04 19:30:01 -0400

    add CSS baseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import logo from './logo.svg';
 import NavBar from 'ui/atoms/navigation/NavBar';
@@ -12,41 +13,44 @@ import './App.css';
 function App() {
   useAuthPersistence();
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <NavBar />
+    <>
+      <CssBaseline />
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <NavBar />
+        </Grid>
+        <Switch>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/passwordreset">
+            <SignUp />
+          </Route>
+          <Route path="/profile">
+            <ProfilePage />
+          </Route>
+          <Route path="/">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </Route>
+        </Switch>
       </Grid>
-      <Switch>
-        <Route path="/signin">
-          <SignIn />
-        </Route>
-        <Route path="/signup">
-          <SignUp />
-        </Route>
-        <Route path="/passwordreset">
-          <SignUp />
-        </Route>
-        <Route path="/profile">
-          <ProfilePage />
-        </Route>
-        <Route path="/">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </Route>
-      </Switch>
-    </Grid>
+    </>
   );
 }
 
