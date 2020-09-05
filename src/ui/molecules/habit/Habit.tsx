517939b8ed44023c881cf64a648ef97b21489411
Aarron Lee <aclee91@gmail.com>
2020-09-05 16:44:43 -0400

    add flex-wrap styles to habits

--- src/ui/molecules/habit/Habit.tsx
+++ src/ui/molecules/habit/Habit.tsx
@@ -1,14 +1,21 @@
 import React, { FunctionComponent } from 'react';
 import Calendar from 'ui/molecules/calendar/Calendar';
 import MonthNav from 'ui/molecules/calendar/MonthNav';
+import useHabit from 'redux-modules/habits/hooks/useHabit';
 
 type HabitProps = {
   habitId: string;
 };
 
 const Habit: FunctionComponent<HabitProps> = ({ habitId }) => {
+  const habit: { name: string; history: object | undefined } = useHabit(habitId);
+  if (!habit) {
+    return null;
+  }
+  const { name, history = {} } = habit;
   return (
     <div>
+      {name}
       <MonthNav>
         {({ month, year }: { month: number; year: number }) => (
           <Calendar month={month} year={year} />
--- src/ui/molecules/habits/Habits.tsx
+++ src/ui/molecules/habits/Habits.tsx
@@ -2,15 +2,27 @@ import React, { FunctionComponent } from 'react';
 import CreateHabitDialog from 'ui/molecules/dialogs/CreateHabitDialog';
 import useHabitIds from 'redux-modules/habits/hooks/useHabitIds';
 import Habit from '../habit/Habit';
+import { css } from 'emotion';
+
+const habitContainerStyles = css`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  max-width: 1000px;
+  align-items: center;
+  justify-content: center;
+`;
 
 const Habits: FunctionComponent = () => {
   const habitIds = useHabitIds();
   return (
     <div>
       <CreateHabitDialog />
-      {habitIds.map((habitId: string, idx: number) => {
-        return <Habit habitId={habitId} key={idx} />;
-      })}
+      <div className={habitContainerStyles}>
+        {habitIds.map((habitId: string, idx: number) => {
+          return <Habit habitId={habitId} key={idx} />;
+        })}
+      </div>
     </div>
   );
 };
