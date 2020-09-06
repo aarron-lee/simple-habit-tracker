0e1aa1f00e8e777ee619c346a1327a7f0b7efdbe
Aarron Lee <aclee91@gmail.com>
2020-09-05 21:23:24 -0400

    register serviceWorker if prd

--- src/index.tsx
+++ src/index.tsx
@@ -34,4 +34,6 @@ document.addEventListener('DOMContentLoaded', initApp);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if (process.env.NODE_ENV === 'production') {
+  serviceWorker.register();
+}
