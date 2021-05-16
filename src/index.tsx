9a7a97ae2fc3c93bcb536946b2ceea764e1f084f
Aarron Lee <aclee91@gmail.com>
2021-05-16 15:24:13 -0400

    add updated serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import createStore from 'redux-modules/store';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import HTML5toTouch from 'react-dnd-multi-backend/dist/esm/HTML5toTouch';
@@ -39,6 +38,4 @@ document.addEventListener('DOMContentLoaded', initApp);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-if (process.env.NODE_ENV === 'production') {
-  serviceWorkerRegistration.register();
-}
+serviceWorkerRegistration.register();
