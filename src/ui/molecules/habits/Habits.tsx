79fb7918a1c540b5f84622e1c2041c3a6fe356bc
Aarron Lee <aarron-lee@users.noreply.github.com>
2025-09-29 15:55:56 -0400

    fix: improve responsive styling for week view

--- src/ui/molecules/habits/Habits.tsx
+++ src/ui/molecules/habits/Habits.tsx
@@ -1,15 +1,15 @@
-import React, { FunctionComponent } from 'react';
-import useHabitIds from 'redux-modules/habits/hooks/useHabitIds';
-import Habit from '../habit/Habit';
-import { css } from 'emotion';
-import { Flipper, Flipped } from 'react-flip-toolkit';
-import useUpdateHabitViewType from 'redux-modules/session/hooks/useUpdateHabitView';
-import useHabitViewType from 'redux-modules/session/hooks/useHabitViewType';
+import React, { FunctionComponent } from "react";
+import useHabitIds from "redux-modules/habits/hooks/useHabitIds";
+import Habit from "../habit/Habit";
+import { css } from "emotion";
+import { Flipper, Flipped } from "react-flip-toolkit";
+import useUpdateHabitViewType from "redux-modules/session/hooks/useUpdateHabitView";
+import useHabitViewType from "redux-modules/session/hooks/useHabitViewType";
 
 const HABIT_VIEW_TYPE = {
-  week: 'week',
-  month: 'month',
-}
+  week: "week",
+  month: "month",
+};
 
 const habitContainerStyles = css`
   display: flex;
@@ -22,50 +22,53 @@ const habitContainerStyles = css`
   }
 `;
 
-const weekHabitContainerStyles = css`
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  max-width: 1000px;
-  align-items: center;
-  @media only screen and (max-width: 1000px) {
-    justify-content: center;
-  }
-`;
+// const weekHabitContainerStyles = css`
+//   display: flex;
+//   flex-direction: column;
+//   flex-wrap: wrap;
+//   max-width: 1000px;
+//   align-items: center;
+//   @media only screen and (max-width: 1000px) {
+//     justify-content: center;
+//   }
+// `;
 
 const useToggleHabitViewType = () => {
-  const habitViewType = useHabitViewType()
+  const habitViewType = useHabitViewType();
 
-  const updateHabitViewType = useUpdateHabitViewType()
+  const updateHabitViewType = useUpdateHabitViewType();
   const toggleHabitViewType = () => {
     if (habitViewType === HABIT_VIEW_TYPE.month) {
-      updateHabitViewType({ habitViewType: HABIT_VIEW_TYPE.week })
+      updateHabitViewType({ habitViewType: HABIT_VIEW_TYPE.week });
+    } else {
+      updateHabitViewType({ habitViewType: HABIT_VIEW_TYPE.month });
     }
-    else {
-      updateHabitViewType({ habitViewType: HABIT_VIEW_TYPE.month })
-    }
-  }
+  };
 
-  return { habitViewType, toggleHabitViewType }
-}
+  return { habitViewType, toggleHabitViewType };
+};
 
 const Habits: FunctionComponent = () => {
   const habitIds = useHabitIds();
 
-  const { habitViewType, toggleHabitViewType } = useToggleHabitViewType()
+  const { habitViewType, toggleHabitViewType } = useToggleHabitViewType();
 
   return (
     <>
       <button onClick={toggleHabitViewType}>toggle habit view</button>
       {habitIds.length > 0 && (
-        <Flipper flipKey={habitIds.join('')}>
-          <div className={habitViewType === HABIT_VIEW_TYPE.week ? weekHabitContainerStyles : habitContainerStyles}>
+        <Flipper flipKey={habitIds.join("")}>
+          <div className={habitContainerStyles}>
             {habitIds.map((habitId: string, idx: number) => {
               return (
                 <Flipped key={habitId} flipId={habitId}>
                   {/* <Flipped/> needs a div child */}
                   <div>
-                    <Habit habitId={habitId} order={idx} habitViewType={habitViewType} />
+                    <Habit
+                      habitId={habitId}
+                      order={idx}
+                      habitViewType={habitViewType}
+                    />
                   </div>
                 </Flipped>
               );
